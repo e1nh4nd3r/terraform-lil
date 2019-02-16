d1472ac88bc329b23e8403f4f7c0eed1b731963f
James Goin <jgoin@jgoin.net>
2019-02-15 19:36:11 -0500

    Create two subnets, create ingress security group, allow tcp 80

--- resources.tf
+++ resources.tf
@@ -6,3 +6,34 @@ resource "aws_vpc" "environment-example-one" {
     Name = "terraform-aws-vpc-example-one"
   }
 }
+
+resource "aws_subnet" "subnet1" {
+  cidr_block = "${cidrsubnet(aws_vpc.environment-example-one.cidr_block, 3, 1)}"
+  vpc_id = "${aws_vpc.environment-example-one.id}"
+  availability_zone = "us-east-1a"
+  tags {
+    Name = "terraform-aws-subnet-exmaple-one"
+  }
+}
+
+resource "aws_subnet" "subnet2" {
+  cidr_block = "${cidrsubnet(aws_vpc.environment-example-one.cidr_block, 2, 2)}"
+  vpc_id = "${aws_vpc.environment-example-one.id}"
+  availability_zone = "us-east-1b"
+  tags {
+    Name = "terraform-aws-subnet-exmaple-one"
+  }
+}
+
+resource "aws_security_group" "subnetsecurity" {
+  vpc_id = "${aws_vpc.environment-example-one.id}"
+
+  ingress {
+    cidr_blocks = [
+    "${aws_vpc.environment-example-one.cidr_block}"
+    ]
+    from_port = 80
+    protocol = "tcp"
+    to_port = 80
+  }
+}
